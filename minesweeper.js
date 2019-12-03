fb64b75235de0c64ef1f388b9f7ce1db04db6ff1
Alfonso Rognoni <alfonsorognoni@gmail.com>
2019-12-02 21:34:34 -0300

    check mine : is a mine

--- minesweeper.js
+++ minesweeper.js
@@ -11,7 +11,8 @@ class Minesweeper {
 
     start() {
         this.shuffleBoard();
-        this.drawBoard()
+        this.drawBoard();
+        this.listenCells();
     }
 
     shuffleBoard() {
@@ -23,11 +24,29 @@ class Minesweeper {
         this.board.forEach(element => {
             const cell = document.createElement('div');
             cell.classList.add('cell')
+            cell.innerHTML = '<div></div>'
+
             if (element.mine) {
-                cell.innerHTML = '*'
+                cell.innerHTML = '<div>*</div>'
             }
             this.container.appendChild(cell);
         });
+    }
+
+    listenCells() {
+        const cells = this.container.querySelectorAll('.cell');
+        cells.forEach(element => {
+            element.addEventListener('click', event => {
+                event.target.classList.add('clicked');
+                this.checkMine(event.target);
+            })
+        });
+    }
+
+    checkMine(cell) {
+        if (cell.innerHTML === '*') {
+            alert('BOOOM!');
+        }
     }
 }
 export default Minesweeper;
